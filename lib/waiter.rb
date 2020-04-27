485ac9bb36d07a2c8ff8766e0d12ee6fcf1d2ca6
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-27 01:44:30 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -23,7 +23,8 @@ def meals
 end
 
 def best_tipper
-  best_tipped_meal = meals.max {|meal_a, meal_b| meal_a.tip<=> meal_b.tip}
-end
+  best_tipped_meal = meals.max do |meal_a, meal_b|
+    meal_a.tip <=> meal_b.tip
+  end
 
 end
