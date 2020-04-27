77ab5088505c7f0b08676730375dffd14312815d
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-27 01:24:26 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,5 +1,19 @@
 #customer to meal, waiter to meal, so customer to waiter
 
 class Meal
+attr_accessor :waiter, :customer, :total, :tip 
 
+@@all = []
+
+def initialize(waiter, customer, total, tip=0)
+  @waiter = waiter
+  @customer = customer
+  @total = total 
+  @tip = tip
+  @@all << self
+end
+
+def self.all
+  @@all 
+end
 end
